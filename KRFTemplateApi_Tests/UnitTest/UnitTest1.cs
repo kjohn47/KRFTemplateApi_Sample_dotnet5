d7411bc62388028d76fd5ed3513de4a35b11dbcd
Joao Morgado <joaomorgado0@gmail.com>
2021-02-04 19:21:54 +0000

    +Add: added unit tests project

---
+++ KRFTemplateApi_Tests/UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KRFTemplateApi.UnitTest
+{
+    [TestClass]
+    public class BaseUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
